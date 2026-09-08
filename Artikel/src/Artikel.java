bb153291d09261cbb7bda01dce05188c779df576
Kato-Kami <167090800+Kato-Kami@users.noreply.github.com>
2026-09-08 17:21:06 +0200

    Update Artikel.java

--- Artikel/src/Artikel.java
+++ Artikel/src/Artikel.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-enum lebensmittelgruppe {Obst, Gemuese, Fleisch, Suesswaren}
 
+enum lebensmittelgruppe {Obst, Gemuese, Fleisch, Suesswaren}
 public class Artikel {
     public String artikelbezeichnung;
     private int artikelnummer;
@@ -13,16 +13,18 @@ public class Artikel {
         System.out.println("\nArtikelbezeichnung: ");
         artikelbezeichnung = input.next();
 
+        // Auswahl über die Art des Lebensmittels
         // gruppe = lebensmittelgruppe.valueOf(input.nextLine());
         System.out.println("Lebensmittelgruppe (1 = Obst, 2 = Gemuese, 3 = Fleisch, 4 = Suesswaren): ");
         int gruppenZahl = Integer.parseInt(input.next());
         gruppe = lebensmittelgruppe.values()[gruppenZahl-1];
 
-
+        // Eingabe der Artikelnummer
         //artikelnummer = Integer.parseInt(input.nextLine());
         System.out.println("Artikelnummer: ");
         artikelnummer = Integer.parseInt(input.next());
 
+        // Eingabe des Preises
         // verkaufspreis = Double.parseDouble(input.nextLine());
         System.out.println("Preis: ");
         verkaufspreis = Double.parseDouble(input.next());
@@ -57,6 +59,7 @@ public class Artikel {
         this.artikelnummer = artikelnummer;
     }
 
+    // Ermittelt den Artikel mit dem höchsten Verkaufspreis
     public static void teuersterArtikel(Artikel[] artikel) {
         String teuersterArtikel = artikel[0].getArtikelbezeichnung();
         double hoechsterverkaufspreis = artikel[0].getPreis();
@@ -71,6 +74,7 @@ public class Artikel {
         System.out.println(teuersterArtikel);
     }
 
+    // Gibt alle Eigenschaften des Artikels als Text zurück
     @Override
     public String toString () {
         return "Bezeichnung " + artikelbezeichnung + "\nLebenmittelgruppe (1 = Obst, 2 = Gemuese, 3 = Fleisch, 4 = Suesswaren): " + gruppe + "\nArtikelnummer: " + artikelnummer + "\nPreis: " + verkaufspreis + "\n";
