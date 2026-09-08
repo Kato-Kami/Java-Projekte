f52260d4afc549c9c966cd550bea281c14037307
Kato-Kami <167090800+Kato-Kami@users.noreply.github.com>
2026-09-08 18:08:09 +0200

    Update Person.java

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,4 +1,6 @@
 public class Person {
+    
+    // Eigenschaften einer Person
     private String name;
     private int alter;
 
@@ -7,12 +9,14 @@ public class Person {
         this.alter = alter;
     }
 
+    // Überprüft, ob das Alter im gültigen Bereich liegt (Alter nur zwischen 1 und 150 erlaubt)
     public void verifiziereAlter() throws UngueltigesAlterException {
         if (alter < 0 || alter > 150) {
             throw new UngueltigesAlterException ("Das Alter darf nicht negativ, noch die Altersgrenze von 150 Jahren überschreiten.");
         }
     }
 
+    // Gibt die Eigenschaften der Person als Text zurück
     @Override
     public String toString() {
         return "Name: " + name + ", Alter: " + alter;
@@ -24,10 +28,14 @@ public class Person {
 
         try {
             person.verifiziereAlter();
+
+            // Wird nur ausgeführt, wenn keine Exception auftritt
             System.out.println("Dein Alter ist gültig");
             System.out.println(person);
 
         }
+
+        // Fehlermeldung ausgeben, falls eine Exception aufgetreten ist
         catch (Exception e) {
             System.out.println("Fehler: " + e.getMessage());
         }
