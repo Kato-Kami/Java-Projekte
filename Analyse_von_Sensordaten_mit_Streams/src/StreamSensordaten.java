bfabfa1e4983eb431619f2a85157136802ff7874
Kato-Kami <167090800+Kato-Kami@users.noreply.github.com>
2026-09-08 15:50:18 +0200

    Update StreamSensordaten.java

--- Analyse_von_Sensordaten_mit_Streams/src/StreamSensordaten.java
+++ Analyse_von_Sensordaten_mit_Streams/src/StreamSensordaten.java
@@ -12,19 +12,23 @@ public class StreamSensordaten {
                 new Messwert("S1", Instant.parse("2026-01-20T10:00:04Z").toEpochMilli(), 21.5)
         );
 
+        // Prüft, ob alle Temperaturen zwischen 50 °C und 80 °C liegen
         boolean alleGueltig = messwerte.stream().allMatch(m -> m.getTemperatur() >= 50.0 && m.getTemperatur() <= 80.0);
 
         System.out.println("Alle Messwerte gültig: " + alleGueltig);
 
+        // Prüft, ob mindestens eine Temperatur über 70 °C liegt
         boolean anomalieVorhanden = messwerte.stream().anyMatch(m -> m.getTemperatur() > 70.0);
 
         System.out.println("Anomalie vorhanden: " + anomalieVorhanden);
 
+        // sortiert Messwerte aufsteigend nach ihrem Zeitstempel
         messwerte.stream()
                 .sorted((a, b) -> Long.compare(a.getZeitstempel(), b.getZeitstempel()))
                 .forEach(m -> System.out.println("Sortierung " + m.getSensorId() + " @ " + m.getZeitstempel())
                 );
 
+        // Paralleler Stream für die Berechnung der Durchschnittstemperatur
         double durchschnittAlle = messwerte.parallelStream()
                 .map(Messwert::getTemperatur)
                 .reduce(
