7b656d5f8e7f39680ff65da2b4fc218b528cecbf
Kato-Kami <167090800+Kato-Kami@users.noreply.github.com>
2026-09-09 02:03:05 +0200

    Update Messwert.java

--- Analyse_von_Sensordaten_mit_Streams/src/Messwert.java
+++ Analyse_von_Sensordaten_mit_Streams/src/Messwert.java
@@ -1,6 +1,15 @@
+// Klasse Messwert definiert, wie genau ein bestimmter Messwer aufgebaut ist, die 
+// später in StreamSensorDaten in einer Liste als Datenklasse verwendet wird
+
 public class Messwert {
+    
+    // Eindeutige ID des Sensors, der den Messwert aufgenommen hat
     private final String sensorId;
+    
+    // Zeitpunkt, zu dem die Messung durchgeführt wurde
     private final long zeitstempel;
+    
+    // Gemessene Temperatur
     private final double temperatur;
 
     public Messwert(String sensorId, long zeitstempel, double temperatur) {
